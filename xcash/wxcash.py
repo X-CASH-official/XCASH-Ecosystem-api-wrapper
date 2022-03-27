b82b79a23904194f1aa499178b272a383828a47b
Animus <lovro@x-network.io>
2022-03-27 15:49:16 +0300

    initial code for WXCASH access

--- xcash/wxcash.py
+++ xcash/wxcash.py
@@ -8,15 +8,26 @@ class wXcash(Helpers):
     def __init__(self, api_key: str):
         super().__init__()
         self.api = "https://api.polygonscan.com/api?"
-        self.wxcash_contract = "&contractaddress=0x235328f864f38a91f0d2282159ea7c7b7c9f7c62"
-        self.foundation_wallet = "&contractaddress=0x03678f2c2c762DC63c2Bb738c3a837D366eDa560"
+        self.wxcash_contract = "0x235328f864f38a91f0d2282159ea7c7b7c9f7c62"
+        self.foundation_wallet = "0x03678f2c2c762DC63c2Bb738c3a837D366eDa560"
         self.api_key = f'&apikey={api_key}'
+
+        self.address = "&address="
+        self.contract_address = '&contractaddress='
 
         ## Modules
         self.stats = "module=stats"
+        self.account = "module=account"
+        self.token = "module=token"
 
         ## action
         self.token_supply = "&action=tokensupply"
+        self.token_balance = "&action=tokenbalance"
+        self.token_supply_hist = "&action=tokensupplyhistory"
+
+        # Other
+        self.tag_latest = f'&tag=latest'
+        self.block_number = f'&blockno= '
 
     @staticmethod
     def result_conversion(result):
@@ -33,8 +44,25 @@ class wXcash(Helpers):
         response = self.get_response(self.url)
         return self.process_response(response)
 
+    def hist_supply(self, block_height: int):
+        """
+        Get historical supply of WXCASh based on block height
+        """
+        self.url = self.api + self.stats + self.token_supply_hist + self.contract_address + self.wxcash_contract + self.block_number + str(
+            block_height) + self.api_key
+        response = self.get_response(self.url)
+        return self.process_response(response)
+
+    def info(self):
+        self.url = self.api + self.token + self.contract_address + self.wxcash_contract + self.api_key
+        response = self.get_response(self.url)
+        return self.process_response(response)
+
+    # https://docs.polygonscan.com/api-endpoints/tokens
+
+
 
 if __name__ == "__main__":
     wxcsah = wXcash(api_key="67F34NNBNR5PFSNW6FVTY7TNG8TF9MV9BH")
-    supply = wxcsah.supply()
+    supply = wxcsah.info()
     pprint(supply)
